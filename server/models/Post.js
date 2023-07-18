39919b5cd81f0884fe7048dcd093657adfe3dab4
Anshuman Bisoyi <ansuman746@gmail.com>
2023-07-18 23:30:26 +0530

    add backend

---
+++ server/models/Post.js
@@ -0,0 +1,50 @@
+import mongoose from 'mongoose';
+
+const postSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      min: 2,
+      max: 50,
+    },
+    location: {
+      type: String,
+    },
+    description: {
+      type: String,
+      required: true,
+      min: 1,
+      max: 280,
+    },
+    picturePath: {
+      type: String,
+    },
+    userPicturePath: {
+      type: String,
+    },
+    likes: {
+      type: Map,
+      of: Boolean,
+    },
+    comments: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
