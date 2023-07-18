b4d9e76e737d7fc0378177504739c64037296726
Anshuman Bisoyi <ansuman746@gmail.com>
2023-07-19 02:06:50 +0530

    setup file structure for frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
+import HomePage from 'scenes/HomePage.jsx';
+import LoginPage from 'scenes/LoginPage.jsx';
+import ProfilePage from 'scenes/ProfilePage.jsx';
 
 function App() {
   return (
     <div className="app">
-  
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
