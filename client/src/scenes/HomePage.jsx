24d3abc73570967b96c90bfdd838d8e4f457b122
Anshuman Bisoyi <ansuman746@gmail.com>
2023-07-20 02:32:17 +0530

    add navbar

--- client/src/scenes/HomePage.jsx
+++ client/src/scenes/HomePage.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from './Navbar';
+import { Box } from '@mui/material';
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <Box><Navbar/></Box>
   )
 }
 
