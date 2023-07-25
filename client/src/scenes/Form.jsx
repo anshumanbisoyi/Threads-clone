f475c3af712baa721f58868b0b3e926ad558e38b
Anshuman Bisoyi <ansuman746@gmail.com>
2023-07-26 00:30:04 +0530

    fix 7

--- client/src/scenes/Form.jsx
+++ client/src/scenes/Form.jsx
@@ -403,7 +403,7 @@ const Form = () => {
 
   const login = async (values, onSubmitProps) => {
     const loggedInResponse = await fetch(
-      "${process.env.REACT_APP_BASE_URL}/auth/login",
+      `${process.env.REACT_APP_BASE_URL}/auth/login`,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
